b9e671fbf7fba6fb2cdcb4f7eb2367da80e8932b
Sam Rendall <sdrendall@gmail.com>
2014-05-11 23:24:07 -0400

    added html and css files for formatting

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/<foo>')
+def hello(foo):
+    return render_template('hello.html',balls=foo,L=range(10))
 
-@app.route('/beer')
+@app.route('/beer1')
 def beer():
     return 'Beer is Good!'
 
-app.run()
+app.run(host='0.0.0.0', debug=True)
